9343a2fff904b1dc5f92c4d0296011252f1b12fa
Nikhil <nikhilch07@gmail.com>
2026-07-11 14:10:43 -0700

    fixed minor bugs in threeSum problem

--- threeSum.js
+++ threeSum.js
@@ -1,35 +1,38 @@
-const threeSum = (arr) => {
-    let result = [];
-    const sortedArray = arr.sort((a,b) => a - b);
-    console.log('SORTED ARRAY', sortedArray);
 
-    for(let i = 0; i < arr.length - 2; i++){
-        
+const threeSum = (arr) => {
+    arr.sort((a,b) => a - b);
+    console.log('SORTED ARRAY', arr);
+
+    let result = [];
+
+    for (let i = 0; i < arr.length - 2; i++){
+        if(i > 0 && arr[i] == arr[i-1]) continue;
         let left = i + 1;
-        let right = arr.length - 1
-        if( i > 0 && arr[i] == arr[i-1]) i++;
+        let right = arr.length - 1;
+
         while(left < right){
-            const sum  = arr[i] + arr[left] + arr[right]
+
+            const sum = arr[i] + arr[left] + arr[right];
             if(sum == 0){
                 result.push([arr[i], arr[left], arr[right]]);
-                while(left < right  && arr[left] == arr[left + 1]) left++;
-                while(left < right && arr[right] == arr[right - 1]) right--;
-                    left++;
-                    right--;
+                while( left < right && arr[left] == arr[left + 1]) left++;
+                while( left < right && arr[right] == arr[right - 1]) right--;
+                left++;
+                right--;
             } else if(sum > 0){
                 right--;
             } else {
                 left++;
             }
+
         }
+
     }
     return result;
 }
 
+const nums1 = [-1, 0, 1, 2, -1, -4];
+const nums2 = [-2, -2, -2, 0, 0, 2, 2]
 
-// Input: nums = [-1, 0, 1, 2, -1, -4]
-// Output: [[-1, -1, 2], [-1, 0, 1]]
-
-const nums = [-1, 0, 1, 2, -1, -4];
-
-console.log('THREE SUM', threeSum(nums))
+console.log('THREE SUM', threeSum(nums1))
+console.log('THREE SUM', threeSum(nums2))
